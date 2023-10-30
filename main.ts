50dee8251ab1f51646e31653363510f62704a8b0
bilal-inksmith <109742973+mbakhtar@users.noreply.github.com>
2023-10-30 09:55:26 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+let bugVisits = 0
+basic.showNumber(bugVisits)
+basic.forever(function () {
+    if (fwdSensors.touch.fwdIsPressed()) {
+        bugVisits += 1
+        fwdSensors.ledRing.fwdSetAllPixelsColour(0xff0000)
+        basic.showNumber(bugVisits)
+        fwdSensors.ledRing.fwdSetAllPixelsColour(0x000000)
+        if (bugVisits > 10) {
+            fwdSensors.ledRing.fwdSetAllPixelsColour(0x00ff00)
+        }
+    }
+})
